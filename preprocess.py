4f3628eb1de339360b5a9bf0f80571fa14c85427
wush303 <73933966+wush303@users.noreply.github.com>
2024-07-23 21:21:28 +0200

    Added preprocessing tool

---
+++ preprocess.py
@@ -0,0 +1,69 @@
+import pymeshlab
+import os
+import sys
+import argparse
+from pathlib import Path
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Preprocess files for use in signed DNE python package. Fill holes, remove unreferenced vertices, etc...")
+    parser.add_argument("input", nargs='+', help="Path to .ply/.obj file(s) or directory containing mesh files")
+    #parser.add_argument("-v", "--visualize", action="store_true", help="Enable visualization (only for single file inputs)")
+    #parser.add_argument("-o", "--output", nargs='?', const="results.csv", default=None, help="Specify output path for results")
+    #parser.add_argument("-b", "--bandwidth", type=float, default=0.08, help="Set the bandwidth for DNE calculation (default: 0.08)")
+    #parser.add_argument("-d", "--distance-type", choices=['Euclidean', 'Geodesic'], default='Euclidean', help="Specify the distance type for calculations (default: Euclidean)")
+    #parser.add_argument("-c", "--cutoff", type=float, default=0, help="Set the cut-off threshold for DNE calculation (default: 0)")
+    return parser.parse_args()
+
+
+def get_file_names(input_paths):
+    file_names = []
+    for path in input_paths:
+        p = Path(path)
+        if p.is_dir():
+            file_names.extend(p.glob('*'))
+        elif p.is_file():
+            file_names.append(p)
+        else:
+            print(str(p) + " is not a file a or a directory")
+    return [f for f in file_names if f.suffix in ('.ply', '.obj')]
+
+
+def preprocess_file(file_name):
+    ms = pymeshlab.MeshSet()
+    ms.load_new_mesh(file_name)
+    ms.meshing_remove_duplicate_vertices()
+    ms.meshing_remove_duplicate_faces()
+    ms.meshing_remove_folded_faces()
+    ms.meshing_remove_null_faces()
+    ms.meshing_remove_unreferenced_vertices()
+    ms.save_current_mesh(file_name)
+    ms.meshing_close_holes(
+            maxholesize=10000000,
+            selected=False,
+            newfaceselected=True, 
+            selfintersection=False,
+            refinehole=True,
+        )
+    base_name, extension = os.path.splitext(file_name)
+    new_file_name = f"{base_name}_watertight{extension}"
+    ms.save_current_mesh(new_file_name)
+
+
+def main():
+    args = parse_arguments()
+    file_names = get_file_names(args.input)
+
+    if not file_names:
+        print("No .ply or .obj files found in the specified input(s).")
+        sys.exit(1)
+
+    for file_name in file_names:
+        print(file_name)
+        preprocess_file(str(file_name))
+
+    print("filled holes on " + str(len(file_names)) + " files")
+
+
+if __name__ == '__main__':
+    main()
